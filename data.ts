c223bd7c60debc92b1cb2f4ac5c0238b39b5d38e
Pentarakis <Konstantin.Pentarakis@adesso.de>
2019-10-29 14:20:14 +0100

    Dummy Data for characters and books

---
+++ data.ts
@@ -0,0 +1,10 @@
+export const BOOKS = [
+  {id: 3, name: 'A Storm of Swords', isbn: '978-0553106633'},
+  {id: 4, name: 'The Hedge Knight', isbn: '978-0976401100'},
+  {id: 5, name: 'A Feast for Crows', isbn: '978-0553801507'}
+];
+
+export const CHARACTERS = [
+  { id: 1, name: 'Daenerys Targaryen', culture: 'Valyrian'},
+  { id: 2, name: 'Jon Snow', culture: 'Northmen'}
+];
