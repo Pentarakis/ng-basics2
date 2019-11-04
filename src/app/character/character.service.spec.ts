3ea651a5fc1da5837ef9145ed01b44c77af5e383
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-11-04 14:50:28 +0100

    HttpClient-Tests

--- src/app/character/character.service.spec.ts
+++ src/app/character/character.service.spec.ts
@@ -1,15 +1,51 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CharacterService } from './character.service';
+import { Character } from './model/character';
 
 describe('CharacterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
-  }));
+  let httpTestingController: HttpTestingController;
+  let service: CharacterService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(CharacterService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
-    const service: CharacterService = TestBed.get(CharacterService);
     expect(service).toBeTruthy();
   });
+
+  describe('readAll(): Observable<Character[]>', () => {
+    it('should send a GET request to the backend on subscribe', () => {
+      // GIVEN
+      const responseData = TEST_CHARACTERS;
+      const success = jest.fn();
+
+      // WHEN
+      service.readAll().subscribe(success);
+
+      // THEN
+      const req = httpTestingController.expectOne('http://localhost:3000/characters?_start=20&_end=120');
+
+      expect(req.request.method).toEqual('GET');
+      req.flush(responseData);
+      expect(success).toHaveBeenCalledWith(responseData);
+    });
+
+  });
+
+  const TEST_CHARACTERS: Character[] = [{
+    'id': 1,
+    'name': '',
+    'culture': 'Braavosi'
+  }];
 });
